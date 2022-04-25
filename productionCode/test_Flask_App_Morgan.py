5c4feec465f419ac0de758f8538d943858ece097
Morgan <morgan@Blues-Mac-mini.local>
2022-04-25 00:04:55 -0500

    reestablished original folder order

--- productionCode/test_Flask_App_Morgan.py
+++ productionCode/test_Flask_App_Morgan.py
@@ -2,8 +2,8 @@ from Flask_app.Morgans_basic_app import *
 import unittest
 
 class TestFlaskApp(unittest.TestCase):
-    def setUp(self):
-        app.config['TESTING'] = True
+    def setUp(self): 
+        app.config['TESTING'] = True  #seems necessary to end process
         self.app = app.test_client()
     
     def test_home(self):
@@ -13,9 +13,10 @@ class TestFlaskApp(unittest.TestCase):
         "/Products/brandName<br>Replace brandName with the brand you wish to query.<br><br>" \
         "Please note: Case matters, as does including spaces. If the brand is more than one word" \
         "replace the<br>spaces with %20 in the URL.<br> I.e., FRESH & EASY --> /Products/FRESH%20&%20EASY"
-        # expectedResult.encode('utf-8')
-        self.assertIn("You", response.data.decode('utf-8'))
-        # self.assertEqual(expectedResult, response.data.decode('utf-8'))
+        expectedResult.encode('utf-8')
+        # self.assertIn("You", response.data.decode('utf-8'))
+        self.assertEqual(expectedResult, response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
