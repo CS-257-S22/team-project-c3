ab72528684c0cdd7d63201c5fa80ce315d259cce
Morgan <morgan@Blues-Mac-mini.local>
2022-04-24 23:08:00 -0500

    tests

---
+++ productionCode/tests/test_Flask_App_Morgan.py
@@ -0,0 +1,22 @@
+from Flask_app.Morgans_basic_app import *
+import unittest
+
+class TestFlaskApp(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+    
+    def test_home(self):
+        response = self.app.get('/', follow_redirects = True)
+        expectedResult = "You have made it to the homepage for what2Eat, welcome :D.<br><br>" \
+        "If you would like to return a list of products based on brand, please complete the URL with:<br>" \
+        "/Products/brandName<br>Replace brandName with the brand you wish to query.<br><br>" \
+        "Please note: Case matters, as does including spaces. If the brand is more than one word" \
+        "replace the<br>spaces with %20 in the URL.<br> I.e., FRESH & EASY --> /Products/FRESH%20&%20EASY"
+        # expectedResult.encode('utf-8')
+        self.assertIn("You", response.data.decode('utf-8'))
+        # self.assertEqual(expectedResult, response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    unittest.main()
