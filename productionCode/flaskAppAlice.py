214da26af0da904b03ba84c1f9977a2ed5a2df8d
Alice-Cutter <cuttera@carleton.edu>
2022-04-25 19:06:35 -0500

    Alice's TESTS for Flask App

--- productionCode/flaskAppAlice.py
+++ productionCode/flaskAppAlice.py
@@ -1,34 +1,49 @@
 from flask import Flask 
 import csv 
-import sys 
+
 import what2Eat 
 
-#need to import from what2EatModule
 app = Flask (__name__)
 
-#intialize data 
+#intializing data 
 what2EatObject = what2Eat.ProductData("SmallProductSheet.csv")
 
 def get_all_products (brandName):
-    return f"This is the list of all products for the entered brand:{brandName}<br>{what2EatObject.getAllProducts(brandName)}"
-
+    """Function to collect all of the products from the what2EatObject using helper method getAllProducts
+    Input: brandName that you are looking for products with.
+    Output: text that will be printed to screen as well as all of the products for the given brand. """
+    return what2EatObject.getAllProducts(brandName)
+  
 
 @app.route('/Products/<brandName>')
 def get_products_to_screen(brandName):
-     return get_all_products(brandName)
-
+    """Function to call get_all_products and return the list of products. 
+    Input: brandName and correct routing through URL
+    Output: all of the products of a given brandname
+    """
+    return get_all_products(brandName)
+     
 
 @app.errorhandler(404)
 def page_not_found(e):
     "Error screen for when the format of the URL is not correct "
     return "Page not found :(. It seems as if the URL you have entered is not correct. To return to the homepage delete everything in the URL after the first \.  "
+
+
+
 @app.route('/')
 def homepage():
-    "Homepage for App. Gives user the instructions they need to navigate the application"
+    """Home page for app. Gives user the instructions they need to navigate the application
+        Input: Users must not have anything beyond base url in the search bar. 
+        Output: Returns a statement helping users figure out how to navigate website"""
+        
     
-    return "Welcome to the homepage of What2Eat!. This website returns all of the products of a specified brand <br><br>"\
+    return 'Welcome to the homepage of What2Eat!. This website returns all of the products of a specified brand <br><br>"\
             "To get a list of products by a given brand enter the following into the search bar after the homepage URL <br><br>"\
-            "/Products/[Brand Name] (Please note that the URL is case sensitive so be carefull when you type it in.)"
+            "/Products/[Brand Name] (Please note that the URL is case sensitive so be carefull when you type it in.)'
+
+
+
 
 
 
