650fd9a0d6adffb82c66114e3207803d438335f8
Morgan Graves <gravesm@hawkeye.fastmail.net>
2022-05-19 11:45:29 -0500

    connected the database to flask app

--- FrontEndDraft/datasource.py
+++ FrontEndDraft/datasource.py
@@ -15,29 +15,74 @@ class DataSource:
             exit()
         return connection
 
+    def stripCommas(self, rawDataTuples):
+        '''Strip out the leading and trailing parantheses and apostrophes
+        WORK IN PROGRESS'''
+        # rawDataString = ''.join([str(item) for item in rawDataList])
+        # print(rawDataString)
+        # rawDataList = list(rawDataTuples)
+        # rawDataString = rawDataString.replace("(", "")
+        # rawDataString = rawDataString.replace("'", "")
+        # rawDataString = rawDataString.replace(")", "")
+        # print(rawDataString)
+        # for aTuple in rawDataTuples:
+
+        # print(rawDataTuples)
+        # for component in rawDataList:
+        #     component = component.strip(",")
+        
+        # strippedDataList = rawDataString.strip(",")
+      #  strippedDataList = []
+       # for item in rawDataTuples:
+        #    strippedDataList.append(str(item))
+
+        #for item in strippedDataList:
+         #   item = item.replace("(", '')
+          #  item = item.replace("'", '')
+           # item = item.replace(")", '')
+            # appenedItem = item[2: item.length()]
+       # return strippedDataList
+
+    def stripProducts(self, products):
+        '''Strip the prodcuts of leading parantheses and apostrophes'''
+        return self.stripCommas(products)
+
     def getProducts(self, brand):
         '''Gather the products associated with the given brandName'''
 
         cursor = self.theConnection.cursor()
-
         query = "SELECT productName FROM products WHERE brandName=%s"
-
         cursor.execute(query, (brand,))
-        
-        print(cursor.fetchall())
+        brandProducts = (cursor.fetchall())
+        return brandProducts
+      #  return self.stripProducts(brandProducts)
 
     def getAllProducts(self):
+        '''Gather every product name included in the dataset'''
 
         cursor = self.theConnection.cursor()
-
         query = "SELECT productName FROM products"
-
         cursor.execute(query,)
+        allProducts = (cursor.fetchall())
+        return allProducts
+        #return self.stripProducts(allProducts)
+    
+    def getIngredients(self, theProduct):
+        '''Gathers the list of ingredients from the given product'''
 
-        print(cursor.fetchall())
+        cursor = self.theConnection.cursor()
+        query = "SELECT ingredients FROM products WHERE productName=%s"
+        cursor.execute(query, (theProduct,))
+        allProducts = (cursor.fetchall())
+        return allProducts
+
+
+
+        
 
 
 if __name__ == '__main__':
     my_source = DataSource()
     my_source.connect()
-    my_source.getProducts("ACME MARKETS")
+    print(my_source.getProducts("Holiday Candy Corp, Inc."))
+    print(my_source.getIngredients("JELLY WREATHS"))
--- FrontEndDraft/front_end.py
+++ FrontEndDraft/front_end.py
@@ -11,6 +11,7 @@ databaseQuery = DataSource() #initializes new object of datasource
 def homepage():
     return render_template('index.html', rows=get_all_products())
 
+'''We are temporarily keeping this commented out code for reference'''
 # def load_data():
 #     with open('SampleData.csv', newline='') as f:
 #         reader = csv.reader(f)
@@ -39,29 +40,41 @@ def homepage():
 
 #         return render_template('productInfo.html', product=product, brand=brand, ingredients=ingredients)
 
+def get_ingredients_from_database(theProduct):
+    '''Helper function to retrieve ingredients from database'''
+    theIngredients = databaseQuery.getIngredients(theProduct)
+    return theIngredients
+
 def get_products_from_database(brand):
+    '''Helper function to retrieve the prodcuts of the given brand from the database'''
     theProducts = databaseQuery.getProducts(brand)
     return theProducts
 
 def get_all_products():
+    '''Helper funtion to retrieve all of the products in the database'''
     allProducts = databaseQuery.getAllProducts()
     return allProducts
 
+@app.route('/productInfo')
+def display_product_info_list():
+    '''renders the product info page given the product information'''
+    ingredients = get_ingredients_from_database(request.args['product']) #need to figure out how you pass the product name that was selected in the autofill bar
+    return render_template('productInfo.html', product=product, brand=brand, ingredients=ingredients) #how do you also include the brandname info so the template can display it
+    #if we want autofill bar available on this page, do we need to also pass rows=get_all_products?
+
 @app.route('/aboutPage')
 def display_about_page():
-    return render_template('about.html', rows=get_all_products())
+    return render_template('about.html', rows=get_all_products()) #need rows so that autofill bar is still available
 
 @app.errorhandler(404)
 def page_not_found(e):
-     return render_template('404.html', rows=get_all_products())
+     return render_template('404.html', rows=get_all_products()) #need rows so that autofill bar is still available
 
 @app.errorhandler(500)
 def internal_server_error(e): 
     '''Error screen when an internal server error occures'''
     return "Hmmmm, that didn't seem to work. Use /5111 to return to the homepage and /5111/Products/brandName<br>" \
         "to query the products for the requested brand."
-
-#load_data()
 
 app.run(host='0.0.0.0', port=5111)
 
